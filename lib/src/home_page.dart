16ce516f539d3668053d3a62dec964ed2f52d9bd
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2025-08-27 22:54:24 -0300

    Adicionar tela vazia para exemplo de scaffold

---
+++ lib/src/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:introducao_flutter_gdg/src/scaffold/scaffold_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Introdução Flutter GDG'),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(width: double.infinity),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ScaffoldPage(),
+                  ),
+                );
+              },
+              child: const Text('Scaffold'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
